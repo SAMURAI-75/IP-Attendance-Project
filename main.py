6da4ff587e465161b6c9ec927ba845219ca0fa25
SAMURAI-75 <118457782+SAMURAI-75@users.noreply.github.com>
2023-05-14 03:34:22 +0400

    Update main.py
    Webcam works, registering works, face recognition via terminal works( for me )

--- main.py
+++ main.py
@@ -6,14 +6,14 @@ import os.path
 import datetime 
 import pickle 
 import subprocess 
-
+import numpy as np
 import tkinter as tk
 import cv2
 from PIL import Image, ImageTk
 import face_recognition_models
 
 import util 
-from test import test
+#from test import test
 
 
 #blueprint for creating objects
@@ -22,19 +22,15 @@ class App:
         self.main_window = tk.Tk()
         self.main_window.geometry("1200x520+350+100")
 
-        self.login_button_main_window = util.get_button(self.main_window, 'login', 'green', self.login)
+        self.login_button_main_window = util.get_button(self.main_window, 'login', 'gray', self.login, fg = 'black')
         self.login_button_main_window.place(x=750, y=200)
 
-        self.logout_button_main_window = util.get_button(self.main_window, 'logout', 'red', self.logout)
-        self.logout_button_main_window.place(x=750, y=300)
-
-        self.register_new_user_button_main_window = util.get_button(self.main_window, 'register new user', 'gray',
-                                                                    self.register_new_user, fg='black')
+        self.register_new_user_button_main_window = util.get_button(self.main_window, 'register new user', 'gray',self.register_new_user, fg='black')
         self.register_new_user_button_main_window.place(x=750, y=400)
 
         self.webcam_label = util.get_img_label(self.main_window)
         self.webcam_label.place(x=10, y=0, width=700, height=500)
-
+        
         self.add_webcam(self.webcam_label)
 
         self.db_dir = './db'
@@ -44,8 +40,8 @@ class App:
         self.log_path = ' ./log.txt'
 
     def add_webcam(self, label):
-        if 'cap' not in self._dic_:
-            self.cap = cv2.VideoCapture(2)
+        if 'cap' not in self.__dict__:
+            self.cap = cv2.VideoCapture(0)
 
         self._label = label
         self.process_webcam()
@@ -54,42 +50,43 @@ class App:
         ret, frame = self.cap.read()
 
         self.most_recent_capture_arr = frame
-        img_ = cv2.cvtColor(self.most_recent_capture_arr, cv2.COLOR_BGR2RGB)
-        self.most_recent_capture_arr = frame
+        img_ = cv2.cvtColor(self.most_recent_capture_arr,cv2.COLOR_BGR2RGB)
+        self.most_recent_capture_pil = Image.fromarray(img_)
         imgtk = ImageTk.PhotoImage(image=self.most_recent_capture_pil)
         self._label.imgtk = imgtk
         self._label.configure(image=imgtk)
 
-        self._lavel.after(20, self.process_webcam)
+        self._label.after(20, self.process_webcam)
 
     def login(self):
-        unknown_img_path = './.top.jpg'
 
+        unknown_img_path = './.face.jpg'
+        
         cv2.imwrite(unknown_img_path, self.most_recent_capture_arr)
 
         output = str(subprocess.check_output(['face_recognition, self.db_dir, unknown_img_path']))
         name = output.split(',')[1][:-3]
 
         if name in ['unknown_person', 'no_persons_found']:
-            util.msg_box('Ups...', 'Unkown user. Please register new user or try again.')
+            util.msg_box('Oh no...', 'Unkown user. Please register new user or try again.')
         else:
-            util.msg_bo('Welcome back.', 'Welcome, {}'.format(name))
+            util.msg_box('Welcome back.', 'Welcome, {}'.format(name))
             with open(self.logpath, 'a') as f:
-                f.write('{},{},in\n'.format(name, datetime.datetime.now()))
+                f.write('{},{},\n'.format(name, datetime.datetime.now()))
                 f.close()
         
         os.remove(unknown_img_path)
-        
+
 #Fouzan
 
     def register_new_user(self):
         self.register_new_user_window = tk.Toplevel(self.main_window)
         self.register_new_user_window.geometry("1200x520+370+120")
         
-        self.accept_button_register_new_user_window = util.get_button(self.register_new_user_window, 'Accept', 'green', self.accept_register_new_user)
+        self.accept_button_register_new_user_window = util.get_button(self.register_new_user_window, 'Accept', 'grey', self.accept_register_new_user , fg = 'black')
         self.accept_button_register_new_user_window.place(x = 750, y = 300)
 
-        self.try_again_button_register_new_user_window = util.get_button(self.register_new_user_window, 'Try Again', 'red', self.try_again_register_new_user)
+        self.try_again_button_register_new_user_window = util.get_button(self.register_new_user_window, 'Try Again', 'grey', self.try_again_register_new_user , fg = 'black')
         self.accept_button_register_new_user_window.place(x = 750, y = 300)
 
 
@@ -99,10 +96,12 @@ class App:
 
         self.add_img_to_label(self.capture_label)
 
+        #bard code
         self.entry_text_register_new_user = util.get_entry_text(self.register_new_user_window)
+
         self.entry_text_register_new_user.place(x = 750, y = 150)
 
-        self.text_label_register_new_user = util.get_entry_text(self.register_new_user_window, 'Please, input a username:')
+        self.text_label_register_new_user = util.get_entry_text(self.register_new_user_window)
         self.text_label_register_new_user.place(x = 750, y = 70)
 
 
@@ -115,7 +114,9 @@ class App:
         label.imgtk = imgtk
         label.configure(image=imgtk)
 
-        self.register_new_user_capture = self.most_recent_capture_arr()
+        #bard code
+        self.register_new_user_capture = Image.fromarray(self.most_recent_capture_arr)
+
 
 
     def start(self):
@@ -125,15 +126,13 @@ class App:
     def accept_register_new_user(self):
         name = self.entry_text_register_new_user.get(1.0 , "end-1c")
 
-        cv2.inwrite(os.path.join(self.db_dir , '{}.jpg'.format(name) , self.register_new_user_capture))
+        #bard code
+        cv2.imwrite(os.path.join(self.db_dir , '{}.jpg'.format(name)) , np.array(self.register_new_user_capture))
 
         util.msg_box('Success!' , 'User was registered successfully')
 
         self.register_new_user_window.destroy()
 
-if __name__ = "__main__" #idk what this means
+if __name__ == "__main__": #idk what this means
     app = App()
     app.start()
-            
-        
-        
